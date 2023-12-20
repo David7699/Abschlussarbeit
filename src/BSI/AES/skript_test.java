83bb0587f99c5fa82412f01eebf37e96cee9f9ed
David <David_popp7699@web.de>
2023-12-20 13:32:34 +0100

    Test for AES cbc with image

--- src/BSI/AES/skript_test.java
+++ src/BSI/AES/skript_test.java
@@ -1,11 +1,13 @@
 package BSI.AES;
 
 import static BSI.AES.AESUtils.aesSecretKeySpec;
-import static BSI.AES.AESUtils.getInputLaptop;
+import static BSI.AES.AESUtils.getInputPc;
 import static BSI.AES.AESUtils.getKey128Bit;
 import static BSI.AES.AESUtils.getTime;
 import static BSI.AES.AESUtils.timeInSeconds;
 
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.security.Security;
 import javax.crypto.Cipher;
 import javax.crypto.spec.IvParameterSpec;
@@ -17,25 +19,11 @@ import org.bouncycastle.util.encoders.Hex;
 public class skript_test{
 	static byte[] input;
 	static byte[] input_encrypted;
-	static {
-		try {
-			input = getInputLaptop();
-		} catch (Exception e){
-			System.out.println("File cant be found");
-		}
-	}
+	static Cipher cipher;
+	
 	static byte[] keyBytes = getKey128Bit();
 	static SecretKeySpec key = aesSecretKeySpec(keyBytes);
 	static byte[] iv = Hex.decode("9f741fdb5d8845bdb48a94394e84f8a3");
-	static Cipher cipher;
-	static {
-		try {
-			cipher = Cipher.getInstance("AES/CBC/NoPadding", "BC");
-		} catch (Exception e){
-			System.out.println("Cipher not found");
-		}
-	}
-	
 	
 	public static byte[] encrypt() throws Exception{
 		cipher.init(Cipher.ENCRYPT_MODE, key, new IvParameterSpec(iv));
@@ -51,11 +39,11 @@ public class skript_test{
 	
 	public static void main(String[] args) throws Exception {
 		Security.addProvider(new BouncyCastleProvider());
+		input = Files.readAllBytes(Paths.get("C:\\Users\\David\\Downloads\\bild.jpg"));
+		cipher = Cipher.getInstance("AES/CBC/NoPadding", "BC");
 		long start = getTime();
-		for(int i = 0; i <= 100000; i++) {
-			encrypt();
-			decrypt();
-		}
+		encrypt();
+		decrypt();
 		long end = getTime();
 		System.out.println((end - start) / 1000F);
 	}
